f074b393fe08b36ec41bcde07ab62e10c98cd99f
velkuns <romain.inscription@hotmail.fr>
2020-01-06 18:30:18 +0100

    Initial code:
     * Add base twig helper service
     * Add base trait for controller
     * Add base middleware for app

---
+++ src/Context.php
@@ -0,0 +1,139 @@
+<?php declare(strict_types=1);
+
+/*
+ * Copyright (c) Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eureka\Component\TwigHelper;
+
+/**
+ * Context data collection for Twig template class.
+ *
+ * @author Romain Cottard
+ */
+class Context implements \Iterator
+{
+    /** @var integer $length Length of the collection */
+    protected $length = 0;
+
+    /** @var integer Current position of the cursor in collection. */
+    protected $index = 0;
+
+    /** @var array $indices Index of keys */
+    protected $indices = [];
+
+    /** @var array $collection Collection of data. */
+    protected $collection = [];
+
+    /**
+     * DataCollection constructor.
+     */
+    public function __construct()
+    {
+        $this->collection = [];
+    }
+
+    /**
+     * Add data to the collection.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return $this
+     */
+    public function add(string $key, $value): self
+    {
+        $this->collection[$key]       = $value;
+        $this->indices[$this->length] = $key;
+        $this->length++;
+
+        return $this;
+    }
+
+    /**
+     * Get length of the collection.
+     *
+     * @return int
+     */
+    public function length(): int
+    {
+        return $this->length;
+    }
+
+    /**
+     * Get current data
+     *
+     * @return mixed
+     */
+    public function current()
+    {
+        return $this->collection[$this->indices[$this->index]];
+    }
+
+    /**
+     * Reset internal cursor.
+     *
+     * @return void
+     */
+    public function reset(): void
+    {
+        $this->index = 0;
+    }
+
+    /**
+     * Get current key.
+     *
+     * @return string
+     */
+    public function key()
+    {
+        return $this->indices[$this->index];
+    }
+
+    /**
+     * Go to the next data
+     *
+     * @return void
+     */
+    public function next(): void
+    {
+        $this->index++;
+    }
+
+    /**
+     * Go to the previous data.
+     *
+     * @return void
+     */
+    public function rewind(): void
+    {
+        $this->index = 0;
+    }
+
+    /**
+     * Check if have more data in the collection
+     *
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return ($this->index < $this->length);
+    }
+
+    /**
+     * Convert to array
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $array = [];
+        foreach ($this as $key => $value) {
+            $array[$key] = $value;
+        }
+
+        return $array;
+    }
+}
---
+++ src/Traits/TwigControllerAwareTrait.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+/*
+ * Copyright (c) Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eureka\Component\TwigHelper\Traits;
+
+use Eureka\Component\TwigHelper\Context;
+use Twig\Environment;
+
+/**
+ * Trait TwigControllerAwareTrait
+ *
+ * @author Romain Cottard
+ */
+trait TwigControllerAwareTrait
+{
+    /** @var Environment */
+    private $twig;
+
+    /** @var Context $context Data collection object. */
+    protected $context = null;
+
+    /**
+     * @param Environment $twig
+     * @return void
+     */
+    public function setTwig(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * Get context.
+     *
+     * @return Context
+     */
+    protected function getContext()
+    {
+        if ($this->context === null) {
+            $this->context = new Context();
+        }
+
+        return $this->context;
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     * @throws
+     */
+    protected function render(string $name): string
+    {
+        $template = $this->twig->load($name);
+
+        return $template->render($this->getContext()->toArray());
+    }
+}
